9c9384d93c4142e985fc2056d719ef2843eba52c
Vandana Reddy <vandanar@uoregon.edu>
2022-02-16 13:05:26 -0800

    changes to OoCA

--- pycairo.py
+++ pycairo.py
@@ -10,21 +10,23 @@ surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 300, 200)
 context = cairo.Context(surface)
 
 #color the background
-context.rectangle(0, 0, 300, 200)
+context.rectangle(0, 0, 300, 200) 
 context.set_source_rgb(.76, 1.00, .76)
 context.fill()
 
-#draw a rectangle       #(x0,y0,x1,y1)
-context.rectangle(25, 50, 70, 120)
-context.set_source_rgb(.16, .56, .51)
-context.fill()
-
 #draw line
-context.move_to(100, 110)
-context.line_to(250, 110)
+context.move_to(0,100)
+context.line_to(300, 100)
 context.set_source_rgb(1, 0, 0)
 context.set_line_width(10)
 context.stroke()
+
+#draw a rectangle       #(x0,y0,x1,y1)
+context.rectangle(50, 50, 200, 100)
+context.set_source_rgb(.16, .56, .51)
+context.fill()
+
+
 
 #create png output
 surface.write_to_png('prettypicture.png')
